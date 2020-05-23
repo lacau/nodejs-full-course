6f3315ee9569cb2ba95df1a520a3cc340f5dbf56
lacau <lacau@zpay.com.br>
2020-05-23 12:19:20 -0300

    Add lesson 3

---
+++ lesson-3/app.js
@@ -0,0 +1,6 @@
+const utils = require('./utils.js')
+const notes = require('./notes.js')
+
+console.log(utils.name)
+console.log(utils.sum(5, 5))
+console.log(notes())
---
+++ lesson-3/utils.js
@@ -0,0 +1,10 @@
+const name = 'Lacau'
+
+const sum = (x, y) => {
+    return x + y
+}
+
+module.exports = { 
+    sum: sum, 
+    name: name 
+}
