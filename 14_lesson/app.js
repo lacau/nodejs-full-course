76ff5deb2ecbf466fc0b734946b868e53665a309
lacau <lacau@zpay.com.br>
2020-05-28 20:21:46 -0300

    Add lesson 14

--- 14_lesson/app.js
+++ 14_lesson/app.js
@@ -1,32 +1,26 @@
-setTimeout(() => {
-    console.log('Two seconds are up')
-}, 2000)
+const add = (numsArray, callback) => {
+    if(!numsArray || numsArray.constructor !== Array) {
+        callback('First argument should be an array')
+        return
+    }
+    if(numsArray.length <= 1) {
+        callback('First argument array length should be equal or greater than 2')
+        return
+    }
 
-const names = ['Lacau', 'Jen', 'Jess']
-const shortNames = names.filter((name) => {
-    return name.length <= 4
-})
+    var sum = 0
+    for(var i = 0; i < numsArray.length; i++) {
+        sum += numsArray[i]
+    }
 
-console.log(shortNames)
-
-const mySetTimeout = async (callback, miliseconds) => {
-    await setTimeout(callback, miliseconds);
+    callback(undefined, sum)
 }
 
-const test = () => {
-    mySetTimeout(() => {
-        console.log('Five seconds are up')
-    },5000)
-}
+add([2, 8, 10], (error, sum) => {
+    if(error) {
+        console.log(error)
+        return
+    }
 
-test()
-
-const add = (num1, num2, callback) => {
-    setTimeout(() => {
-        callback(num1 + num2)
-    }, 2000)
-}
-
-add(1, 4, (sum) => {
     console.log(sum)
 })
