7b0efbac7580e8fbfcd0516d35d58a33158219ab
lacau <lacau@zpay.com.br>
2020-07-06 20:29:24 -0300

    Add lesson 20

---
+++ 20_lesson/src/app.js
@@ -0,0 +1,52 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Home',
+        name: 'Lacau'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        image: '__aaa.jpg',
+        name: 'Lacau'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: "Help",
+        text: 'Help text explanation',
+        name: 'Lacau'
+    })
+})
+
+const printHeaders = ({ headers }) => {
+    console.log("####################")
+    console.log('#  HEADERS')
+    console.log("####################")
+    console.log(JSON.stringify(headers));
+}
+
+app.listen(3000, () => {
+    console.log('Server started at port 3000')
+})
